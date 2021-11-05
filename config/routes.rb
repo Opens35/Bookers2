b5a64c2aad9056fb7fd5df78d2a45536627edc0d
Opens35 <ryuttti1102@gmail.com>
2021-11-05 00:34:10 +0000

    totyuu

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get "home/about" => "homes#about"
-  resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update]
+  resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update] do
+    resources :favorites, only:[:create, :destroy ]
+  end
   resources :users, only: [:new, :index,:show, :edit, :update, :create]
   
 end
