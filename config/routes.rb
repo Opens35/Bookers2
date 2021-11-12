949daac91e84ba7d221fe811157cfa5042a8129a
Opens35 <ryuttti1102@gmail.com>
2021-11-12 03:18:08 +0000

    follow

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'relationships/followings'
+  get 'relationships/followers'
   devise_for :users
   root to: "homes#top"
   get "home/about" => "homes#about"
@@ -6,6 +8,10 @@ Rails.application.routes.draw do
     resource :favorites, only:[:create, :destroy ]
     resources :book_comments, only:[:create, :destroy]
   end
-  resources :users, only: [:new, :index,:show, :edit, :update, :create]
+  resources :users, only: [:new, :index,:show, :edit, :update, :create] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
   
 end
